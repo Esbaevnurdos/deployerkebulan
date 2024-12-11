69f4651131c18a0e66f386b0cd212d2414bcd61f
Esbaevnurdos <nurdosesbaev82@gmail.com>
2024-12-11 21:58:03 +0500

    hello

--- main.go
+++ main.go
@@ -68,7 +68,7 @@ func getNextID() int {
 // CORS Middleware
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow frontend origin
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500") // Allow frontend origin
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
@@ -81,6 +81,7 @@ func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		next(w, r)
 	}
 }
+
 
 func handleDishes(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
